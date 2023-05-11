2de2ef6c730191f85729c972a273aeb52660db22
Stefano Bortolini <stefanobortolini@hotmail.com>
2023-05-10 21:10:28 -0300

    Tablas creadas

--- TerceraPreEntrega/AppTerceraPreEntrega/models.py
+++ TerceraPreEntrega/AppTerceraPreEntrega/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nombre      = models.CharField(max_length=256)
+    apellido    = models.CharField(max_length=256)
+    email       = models.EmailField(blank=True)
+    telefono    = models.CharField(max_length=20, blank=True)
+    dni         = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.nombre}, {self.apellido}"
+
+class Vendedor(models.Model):
+    username        = models.CharField(max_length=15)
+    nombre          = models.CharField(max_length=256)
+    apellido        = models.CharField(max_length=256)
+    email           = models.EmailField(blank=True)
+    telefono        = models.CharField(max_length=20, blank=True)
+    dni             = models.CharField(max_length=32)
+    fcha_nacimiento = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.nombre}, {self.apellido}, {self.dni}"
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=30)
+    marca = models.CharField(max_length=20)
+    precio = models.IntegerField(max_length=7)
+    stock = models.IntegerField(max_length=4)
+
+    def __str__(self):
+        return f"{self.nombre}, {self.marca}, {self.precio}, {self.stock}"
+
+class Venta(models.Model):
+    username  = models.ForeignKey(Vendedor)
+    cliente   = models.ForeignKey(Cliente)
+    producto  = models.CharField(max_length=50)
+    cantidad  = models.IntegerField(max_length=2)
+    preciotot = models.IntegerField(max_length=9)
+
+    def __str__(self):
+        return f"{self.username}, {self.cliente}, {self.producto}, {self.cantidad}"
